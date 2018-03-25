1eb40118faf93669cc5a4c869434b80e78f050ba
shareactor <yonatan@shareactor.io>
2018-03-25 12:46:18 +0200

    get function name

--- ml/s3_connector.py
+++ ml/s3_connector.py
@@ -2,11 +2,12 @@ import os
 
 import boto3
 
-from ml.utils import get_settings_value, mkdir, mkdirs
+from ml.utils import get_settings_value, mkdir, mkdirs,get_function_name
 
 s3_connection = boto3.client('s3')
 model_bucket = s3_connection.get_bucket(get_settings_value('model_bucket'))
 dataset_bucket = s3_connection.get_bucket(get_settings_value('dataset_bucket'))
+lambda_client = boto3.client('lambda')
 
 
 def download_dir_s3(bucket, local_folder):
@@ -26,3 +27,17 @@ def upload_to_s3(bucketname, path):
         for file in files:
             file_path = os.path.join(root, file)
             s3_connection.upload_file(file_path, bucketname, file_path)
+
+
+
+def update_function_dersion(version=None, function_name=None):
+    if function_name is None:
+        function_name = get_function_name()
+    if version is None or not str(version).isdigit():
+        return None
+    environemnt = {'Variables': {
+        'VERSION': str(version)
+        }
+    }
+    response = lambda_client.update_function_configuration(function_name, Environment=environemnt)
+    return response
