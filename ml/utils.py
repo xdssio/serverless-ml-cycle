c537d6489c712936ac4bd4751841f1b8135a7837
shareactor <yonatan@shareactor.io>
2018-03-25 12:46:27 +0200

    get function name

--- ml/utils.py
+++ ml/utils.py
@@ -24,6 +24,10 @@ def get_settings_value(key, stage='dev'):
     return settings[stage].get(key)
 
 
+def get_environment_variable(key):
+    return os.environ.get(key, None)
+
+
 def mkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -32,3 +36,14 @@ def mkdir(directory):
 def mkdirs(path):
     dirs = path.split('/')
     mkdir('/'.join(dirs[:-1]))
+
+
+def get_pipeline():
+    # TODO
+    return None
+
+
+def get_function_name():
+    stage = get_environment_variable('STAGE')
+    project_name = get_settings_value('project_name', stage)
+    return "-".join([get_environment_variable(project_name, stage)])
