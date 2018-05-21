47f3d2a86ef02bb643dba0edfa8abb25f35d90b1
shareactor <yonatan@shareactor.io>
2018-05-21 20:18:07 +0200

    use config file

--- ml/model.py
+++ ml/model.py
@@ -6,8 +6,8 @@ import boto3
 import pandas as pd
 from sklearn.externals import joblib
 
+import config
 from ml.pipeline import create_pipeline
-from ml.utils import get_settings_value
 
 
 class Model:
@@ -16,12 +16,21 @@ class Model:
         self.updated = self.created
         self.version = -1
         self.pipeline = create_pipeline()
-        self.features = get_settings_value('model_features')
-        self.target = get_settings_value('model_target')
-        self.model_name = model_name if model_name else get_settings_value('model_name')
+        self.features = config.model_features
+        self.target = config.model_target
+        self.model_name = model_name if model_name else config.model_name
 
     def increment(self):
         self.version += 1
+
+    def get_model_key(self, model_name=None, version=None):
+        version = version if version is not None else self.version
+        model_key_name = config.model_key_name
+        model_name = model_name if model_name else self.model_name
+        return '/'.join([model_name, str(version), model_key_name])
+
+    def get_estimator(self):
+        return self.pipeline.steps[-1][1]
 
     def preprocess(self, data):
         # We don't really need this, but for possible use cases
@@ -61,28 +70,30 @@ class Model:
         :return:
         """
         s3 = boto3.client('s3')
-        bucket = get_settings_value('data_bucket')
-        key = get_settings_value('data_key')
+        bucket = config.data_bucket
+        key = config.data_key
         obj = s3.get_object(Bucket=bucket, Key=key)
         df = pd.read_csv(io.BytesIO(obj['Body'].read()))
         return df
 
     def save(self, model_name=None, path=None, upload=True):
-        model_key_name = model_name if model_name else get_settings_value('model_key_name')
+        model_key = config.model_key_name
+        model_name = model_name if model_name else self.model_name
         model_version = str(self.version)
-        model_tmp_folder = get_settings_value('tmp_folder')
-        model_location = path if path is not None else '/'.join([model_tmp_folder, model_name, model_version])
+        model_tmp_folder = config.tmp_folder
+        model_location = path if path else '/'.join([model_tmp_folder, model_name, model_version])
+
         pathlib.Path(model_location).mkdir(parents=True, exist_ok=True)
-        model_path = '/'.join([model_location, model_key_name])
+        model_path = '/'.join([model_location, model_key])
         joblib.dump(self, model_path)
         if upload:
-            self.upload(model_name=model_key_name, model_path=model_path)
+            self.upload(model_name=model_key, model_path=model_path)
         return model_path
 
     def upload(self, model_name=None, model_path=None, bucket=None):
         s3 = boto3.client('s3')
-        bucket = bucket if bucket else get_settings_value('models_bucket')
-        model_name = model_name if model_name else get_settings_value('model_name')
+        bucket = bucket if bucket else config.models_bucket
+        model_name = model_name if model_name else self.model_name
         model_key = self.get_model_key(model_name=model_name)
         resp = s3.put_object(Body=open(model_path, 'rb'), Bucket=bucket, Key=model_key)
         return resp
@@ -90,19 +101,20 @@ class Model:
     @staticmethod
     def download(model_name=None, version=0, model_path=None, bucket=None):
         s3 = boto3.client('s3')
-        model_name = model_name if model_name else get_settings_value('model_name')
-        model_key_name = get_settings_value('model_key_name')
+        model_name = model_name if model_name else config.model_name
+        model_key_name = config.model_key_name
         model_key = '/'.join([model_name, str(version), model_key_name])
-        bucket = bucket if bucket else get_settings_value('models_bucket')
+        bucket = bucket if bucket else config.models_bucket
         try:
             obj_string = s3.get_object(Bucket=bucket, Key=model_key)['Body'].read()
         except:
             raise ValueError('Model name or version are bad')
 
         if model_path is None:
-            tmp_folder = get_settings_value('tmp_folder')
+            tmp_folder = config.tmp_folder
             model_path = '/'.join([tmp_folder, model_key])
         model_location = "/".join(model_path.split('/')[:-1])
+
         pathlib.Path(model_location).mkdir(parents=True, exist_ok=True)
         with open(model_path, "wb") as the_file:
             the_file.write(obj_string)
@@ -112,18 +124,8 @@ class Model:
     @staticmethod
     def load(model_name=None, version=0, model_path=None, bucket=None):
         if model_path is None:
-            print('download')
+            model_name = model_name if model_name else config.model_name
             model_path = Model.download(model_name=model_name, version=version, bucket=bucket)
+
         model = joblib.load(model_path)
         return model
-
-
-def get_model_key(self, model_name=None, version=None):
-    version = version if version is not None else self.version
-    model_key_name = get_settings_value('model_key_name')
-    model_name = model_name if model_name else get_settings_value('model_name')
-    return '/'.join([model_name, str(version), model_key_name])
-
-
-def get_estimator(self):
-    return self.pipeline.steps[-1][1]
